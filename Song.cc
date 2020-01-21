2cebe3d76ac91d6472d8faef3b807a4640a8d617
artur234 <jaskier234@gmail.com>
2020-01-21 20:02:43 +0100

    Finished Song

--- Song.cc
+++ Song.cc
@@ -2,12 +2,24 @@
 
 #include <iostream>
 
+#include "PlayerException.h"
+
 Song::Song(File file) : Track(file) {
-    // throw
+    if (!file.getMetaData("artist").has_value()) {
+        throw new FileException();
+    }
+
+    if (!file.getMetaData("title").has_value()) {
+        throw new FileException();
+    }
 }
 
 void Song::play() {
-    std::cout << "playing song" << std::endl;
+    std::cout << "Song [";
+    std::cout << file.getMetaData("artist").value() << ", ";
+    std::cout << file.getMetaData("title").value() << "]: ",
+    std::cout << file.getContent();
+    std::cout << std::endl;
 }
 
 std::shared_ptr<Track> Song::newTrack(File file) {
