d4227c31bed35b597d92b1422d03193caecae708
artur234 <jaskier234@gmail.com>
2020-01-23 17:49:56 +0100

    Added parsing tests

--- test.cc
+++ test.cc
@@ -3,13 +3,17 @@
 #include "lib_playlist.h"
 
 void cycle_test1();
+void parsing_test1();
+void parsing_test2();
 
 int main() {
-    // cycle_test1();
+    cycle_test1();
 
-    Player player;
+    parsing_test1();
+    parsing_test2();
+    // Player player;
     
-    auto song = player.openFile(File("audio|title:drivers|artist:car|whales"));
+    // auto song = player.openFile(File("audio|title:drivers|artist:car|whales"));
     
     return 0;
 }
@@ -64,6 +68,69 @@ void cycle_test1() {
         std::cout << __FUNCTION__ << ":" << __LINE__ << " failed" << std::endl;
         return;
     }
+    
+    std::cout << __FUNCTION__ << " OK" << std::endl;
+}
+
+void negative_parsing(std::string data) {
+    Player player;
+    
+    try {
+        auto a = player.openFile(File(data));
+        std::cout << "ERR: " << data << "parsed correctly" << std::endl;
+    } catch (PlayerException &e) {
+    
+    }
+}
+
+void parsing_test1() {
+    Player player;
+    
+    // positive
+    
+    auto a1 = player.openFile(File("video|title:asd|year:1234|xyz"));
+    auto a2 = player.openFile(File("audio|title:asd|year:1234|artist:asd|xyz"));
+    auto a3 = player.openFile(File("video|title:asd|year:1234|other metadata:some data with whitespaces|xyz"));
+    auto a4 = player.openFile(File("video|title:asd|year:1234|:empty metadata name|xyz"));
+    auto a5 = player.openFile(File("video|title:asd|year:1234|empty metadata:|xyz"));
+    auto a6 = player.openFile(File("video|multiple:colons:in:metadata|title:asd|year:1234|xyz"));
+    auto a7 = player.openFile(File("video|title:multiple:colons:in:title|year:1234|xyz"));
+    auto a8 = player.openFile(File("video|title:multiple:colons:in:title|year:1234|")); // empty content
+    std::cout << "title should be multiple:colons:in:title" << std::endl;
+    a7->play();
+    
+    // same metadata multiple times
+    std::string file = "video|title:film|";
+    
+    for (int i = 0; i < 1000; i++) {
+        file += "year:" + std::to_string(i) + "|";
+    }
+    
+    file += "content";
+    
+    auto a9 = player.openFile(File(file));
+    std::cout << "year should be 999" << std::endl;
+    a9->play();
+    
+    std::cout << __FUNCTION__ << " OK" << std::endl;
+}
+
+void parsing_test2() {
+    negative_parsing("");
+    negative_parsing("alsudhflaksdhfla");
+    // brak metadanych
+    negative_parsing("audio|lyrics");
+    negative_parsing("audio|title:jest|lyrics");
+    negative_parsing("audio|artist:jest|lyrics");
+    negative_parsing("video|title:jest|film");
+    negative_parsing("video|year:12|film");
+    negative_parsing("video|film");
+    
+    // year should be a number
+    negative_parsing("video|title:asdf|year:asdf|film");
+    negative_parsing("video|title:asdf|year:12sdf|film");
+    negative_parsing("video|title:asdf|year:ser12sdf|film");
+    negative_parsing("video|title:asdf|year:ser12|film");
     
     std::cout << __FUNCTION__ << " OK" << std::endl;
 }
